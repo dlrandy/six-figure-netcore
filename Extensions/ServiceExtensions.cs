b936793f9592c34607f3155751247eff019f9c60
dlrandy <qq1208484996@qq.com>
2024-08-08 20:31:34 +0800

    feat: configuration done

---
+++ Extensions/ServiceExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/**
+ * extension method is inherently a static method.what makes it different from 
+ * other static methods is that it accepts this as the first parameter, and 
+ * this represents the data tyoe of the object which will be using that extension
+ * method.An extension method must be defined inside a static class.
+ * 
+ * extension methods are great for organizing your code and extending functionalities.
+ */
+namespace CompanyEmployees.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+           services.AddCors(options =>
+           {
+               options.AddPolicy("CorsPolicy", builder =>
+                    builder
+                        .AllowAnyOrigin() // WithOrigins("https://example.com")
+                        .AllowAnyMethod() // WithMethods("POST","GET")
+                        .AllowAnyHeader()); // WithHeaders("accept","content-type")
+           });
+
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+
+
+            });
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,80 @@
+﻿using CompanyEmployees.Extensions;
+using Microsoft.AspNetCore.HttpOverrides;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//<-----
+builder.Services.ConfigureCors();
+builder.Services.ConfigureIISIntegration();
+//----->
+
+builder.Services.AddControllers();
+
+/**
+ * WebApplication implements IHost,IApllicationBuilder,and IEndpointRouteBuilder
+ * 
+ * middleware is used to modifying the application's pipeline---handling request and response
+ */
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+//<-----
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else {
+    app.UseHsts();
+}
+
+//---->
+
+app.UseHttpsRedirection();
+
+//<-----
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions() {
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
+//----->
+app.UseAuthorization();
+
+//<-----
+// requestdelegate
+// custom middleware use,map(mapwhen),run
+//app.Use(async (context, next) => {
+//    Console.WriteLine($"Logic before executing the next delegate in the use method");
+//    await next.Invoke();
+//    Console.WriteLine($"Logic after executing the next delegate in the use method");
+//});
+
+//// any middlware component that add after the map method, even we don't use the Run middleware inside the branch 
+//app.Map("/usingmapbranch",builder => {
+//    builder.Use(async (context, next) => {
+//        Console.WriteLine("Map branch logic in the Use method before the next delegate");
+//        await next.Invoke();
+//        Console.WriteLine("Map branch logic in the Use method after the next delegate");
+//    });
+//    builder.Run(async context => {
+//        Console.WriteLine($"Map branch response to the client in the run method");
+//        await context.Response.WriteAsync("Hello fro");
+//    });
+//});
+//app.MapWhen(context => context.Request.Query.ContainsKey("testquerystring"), (IApplicationBuilder obj) => {
+//    obj.Run(async (context) => await context.Response.WriteAsync("Hello from the MapWhen branch."));
+
+//});
+//app.Run(async context => {
+//    Console.WriteLine($"Writing the response to the client in the run method");
+//    await context.Response.WriteAsync("Hello from the middleware component.");
+//});
+
+// ----->
+
+app.MapControllers();
+
+app.Run();
+
